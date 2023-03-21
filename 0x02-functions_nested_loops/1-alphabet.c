5ba8762b669b64a2d28ebf09f8cba04d14f09659
Wissal Erramoud <wissal.erramoud@gmail.com>
2023-03-21 15:45:48 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ * @parameter: describe the parameter
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+
+	_putchar(i);
+
+	}
+	_putchar('\n');
+	return (0);
+}
+
