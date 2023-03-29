ae58448d237f0fd170229ca850e9e67e5e5f67e2
Wissal Erramoud <wissal.erramoud@gmail.com>
2023-03-29 13:33:39 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,12 +11,12 @@ char *_strncat(char *dest, char *src, int n)
 	int deslen = 0;
 	int srclen = 0;
 	int i;
-	
-	for (i = 0 ; dest [i] != '\0' ; i++)
+
+	for (i = 0 ; dest[i] != '\0' ; i++)
 		deslen++;
-	for (i = 0 ; src [i] != '\0' ; i++)
+	for (i = 0 ; src[i] != '\0' ; i++)
 		srclen++;
 	for (i = 0 ; i < n ; i++)
-		dest[deslen+i] = src[i];
+		dest[deslen + i] = src[i];
 	return (dest);
 }
