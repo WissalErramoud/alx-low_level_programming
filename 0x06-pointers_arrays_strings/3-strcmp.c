f94c53db9958d3f93cbd74ad82ebc8ad12ac0cf8
Wissal Erramoud <wissal.erramoud@gmail.com>
2023-03-29 13:19:46 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcmp - compare 2 strings.
+ * @s1: string to be compared.
+ * @s2: string for comparation.
+ * Return: Difference.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	for (i = 0; s1[i] != '\0' || s2 [i] != '\0' ; i++)
+	{
+		if (s1[i] != s2[i]
+		{
+			if (s1[i] < s2[i])
+				return (s1[i] - s2[i])
+			else if (s1[i] > s[i])
+
+				
+		}}
+
+				}
+}
