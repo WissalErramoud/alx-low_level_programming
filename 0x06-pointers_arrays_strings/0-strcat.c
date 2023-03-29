f3e54b0a2e16d8ad1fe7ff42b0d1776140eca4ed
Wissal Erramoud <wissal.erramoud@gmail.com>
2023-03-29 06:57:15 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcat - concatenates 2 strings.
+ * @dest: string with concatenation
+ * @src: string to be concatenated
+ * Return: Destination String.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int deslen = 0;
+	int srclen = 0;
+	int i;
+	
+	for (i = 0 ; dest [i] != '\0' ; i++)
+		deslen++;
+	for (i = 0 ; src [i] != '\0' ; i++)
+		srclen++;
+	for (i = 0 ; i <= srclen ; i++)
+		dest[destlen+i] = src[i];
+	return (dest);
+}
