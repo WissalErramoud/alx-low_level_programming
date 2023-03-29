a62e09ff7cb3a6da5e5e07e49797b7720799171f
Wissal Erramoud <wissal.erramoud@gmail.com>
2023-03-29 07:36:41 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strncat - concatenate strings, defining the size of second string.
+ * @dest: string with concatenation
+ * @src: string to be concatenated
+ * @n: number of characters to be append
+ * Return: Destination String
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int deslen = 0;
+	int srclen = 0;
+	int i;
+	
+	for (i = 0 ; dest [i] != '\0' ; i++)
+		deslen++;
+	for (i = 0 ; src [i] != '\0' ; i++)
+		srclen++;
+	for (i = 0 ; i < n ; i++)
+		dest[deslen+i] = src[i];
+	return (dest);
+}
