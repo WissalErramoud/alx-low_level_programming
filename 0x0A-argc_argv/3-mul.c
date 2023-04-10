67497325082e271d373eacbf5c2984ea099bf64a
Wissal Erramoud <wissal.erramoud@gmail.com>
2023-04-10 08:38:30 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -20,11 +20,17 @@ int main(int argc, char *argv[])
 	}
 
 	/*Get the two numbers from the command line arguments*/
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
+	int num1;
+
+	num1 = atoi(argv[1]);
+	int num2;
+
+	num2 = atoi(argv[2]);
 
 	/*Multiply the two numbers and print the result*/
-	int result = num1 * num2;
+	int result;
+
+	result = num1 * num2;
 
 	printf("%d\n", result);
 	return (0);
