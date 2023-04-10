4cbc2357078133d559e72e57b994cd85e6f6a9d4
Wissal Erramoud <wissal.erramoud@gmail.com>
2023-04-10 08:34:27 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
-  2 #include "main.h"
-  3 #include <stdlib.h>
-  4 
-  5 /**
-  6  * main - a function that multiplies two numbers
-  7  * atoi - a function that converts a string to an integer
-  8  * @argc: number of arguments
-  9  * @argv: array of arguments
- 10  *
- 11  * Return: 0 (Success), 1 (Error)
- 12  */
- 13 int main(int argc, char *argv[]) {
- 14     /*Check if two arguments were provided*/
- 15     if (argc != 3) {
- 16         printf("Error\n");
- 17         return 1;
- 18     }
- 19 
- 20     /*Get the two numbers from the command line arguments*/
- 21     int num1 = atoi(argv[1]);
- 22     int num2 = atoi(argv[2]);
- 23 
- 24     /*Multiply the two numbers and print the result*/
- 25     int result = num1 * num2;
- 26     printf("%d\n", result);
- 27 
- 28     return 0;
- 29 }
-~                                                                                   
-~        
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - a function that multiplies two numbers
+ * atoi - a function that converts a string to an integer
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
+int main(int argc, char *argv[])
+{
+	/*Check if two arguments were provided*/
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	/*Get the two numbers from the command line arguments*/
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
+
+	/*Multiply the two numbers and print the result*/
+	int result = num1 * num2;
+
+	printf("%d\n", result);
+	return (0);
+}
