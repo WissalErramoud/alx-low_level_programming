775fb612e5bb0edba1d905f166784ba8883020c9
WissalErramoud <wissal.erramoud@gmail.com>
2023-06-09 11:24:39 -0700

    Add a header file for directory 0x09 - C static libraries

---
+++ 0x09-static_libraries/main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int _strlen(char s);
+void _puts(chars);
+char _strcpy(chardest, char src);
+int _atoi(chars);
+char _strcat(chardest, char src);
+char_strncat(char dest, charsrc, int n);
+char _strncpy(chardest, char src, int n);
+int _strcmp(chars1, char s2);
+char_memset(char s, char b, unsigned int n);
+char_memcpy(char dest, charsrc, unsigned int n);
+char _strchr(chars, char c);
+unsigned int _strspn(char s, characcept);
+char _strpbrk(chars, char accept);
+char_strstr(char haystack, charneedle);
+
+#endif
