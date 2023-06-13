00f825135cf55e4da6b39adb5ede13a6021fe861
WissalErramoud <wissal.erramoud@gmail.com>
2023-06-13 13:56:44 -0700

    Add Write a function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -23,13 +23,12 @@ char *str_concat(char *s1, char *s2)
 
 	if (strdest == NULL)
 		return (NULL);
-	i = ci = 0;
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 		strdest[i] = s1[i];
 	}
 
-	for (; s2[j] != '\0'; i++; j++)
+	for (j = 0; s2[j] != '\0'; i++, j++)
 	{
 		conct[i] = s2[j];
 
