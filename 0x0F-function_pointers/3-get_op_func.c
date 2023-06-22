37329d6042db366c1a995003a210d2191de12d75
WissalErramoud <wissal.erramoud@gmail.com>
2023-06-21 18:43:07 -0700

    Implement get_op_func function to select the correct operation

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,5 @@
 #include "3-calc.h"
-#include <stdlib.h>
+
 /**
  * get_op_func - Selects the correct function to perform
  *               the operation asked by the user.
@@ -26,3 +26,4 @@ int (*get_op_func(char *s))(int, int)
 
 	return (ops[i].f);
 }
+
